8aacb60df168e51a88705fab98dd22a9e974cf7a
Rebecca Dias <rebeccadias2510@gmail.com>
2023-12-12 23:08:42 -0800

    es6 added

---
+++ JavaScript/ES6/script.js
@@ -0,0 +1,16 @@
+//Basic => syntax
+const multiply = (a, b) => a * b;
+const result = multiply(2, 3);
+console.log(result);
+
+//single parameter, no parathesis
+
+const sqaure = (x) => x * x;
+const sqrfun = sqaure(5);
+console.log(sqrfun);
+
+//no parameters
+const random = () => Math.random();
+console.log(random());
+
+//destructuring 
