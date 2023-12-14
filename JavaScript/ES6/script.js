5a132d6ed09399cec37ed0e1c135799da2d3faf6
Rebecca Dias <rebeccadias2510@gmail.com>
2023-12-14 13:57:49 -0800

    adding react and gitignore

--- JavaScript/ES6/script.js
+++ JavaScript/ES6/script.js
@@ -13,4 +13,21 @@ console.log(sqrfun);
 const random = () => Math.random();
 console.log(random());
 
-//destructuring 
+//assignment
+//question
+//Your task is to create a function extractAuthors for a library management system.
+//This function should take an array of book objects, each with a title and an author property. The function must return an array containing the authors' names. Additionally, your function should validate that each book object has the correct properties. If a book object does not have an author property, your function should throw an error. Use ES6 arrow functions to implement this functionality, ensuring that the function works correctly only when the correct property names are used.
+const extractAuthors = (books) => {
+  if (books.some((book) => !("author" in book))) {
+    throw new Error("Invalid book object - 'author' property missing");
+  }
+  return books.map((book) => book.author);
+};
+
+// Example usage:
+const libraryBooks = [
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "The Catcher in the Rye", author: "J.D. Salinger" },
+];
+
+console.log(extractAuthors(libraryBooks)); // ['Aldous Huxley', 'J.D. Salinger']
